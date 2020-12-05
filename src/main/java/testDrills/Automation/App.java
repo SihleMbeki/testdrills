fae83c30b8b307aeff7938f01074fa9febb25adf
Masakhane Matanzima <sihle.matanzima@gmail.com>
2020-12-05 22:26:48 +0200

    Maven simple archtype

---
+++ src/main/java/testDrills/Automation/App.java
@@ -0,0 +1,9 @@
+package testDrills.Automation;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	
+}
---
+++ src/test/java/testDrills/Automation/testing/AppTest.java
@@ -0,0 +1,12 @@
+package testDrills.Automation.testing;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		assert Boolean.TRUE;
+	}
+	
+}
