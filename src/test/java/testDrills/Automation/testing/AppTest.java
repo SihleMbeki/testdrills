777800c53e82e077f24bf5c9fe3b0d9692541232
Masakhane Matanzima <sihle.matanzima@gmail.com>
2020-12-05 23:42:09 +0200

    TestNG setup

--- src/test/java/testDrills/Automation/testing/AppTest.java
+++ src/test/java/testDrills/Automation/testing/AppTest.java
@@ -1,12 +1,37 @@
 package testDrills.Automation.testing;
 
+import java.lang.reflect.Method;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class AppTest {
+public class AppTest extends Base{
 
-	@Test
-	public void test() {
+	/*Setting test class methods
+	 * 
+	 *Pre-conditions
+	 *Conditions
+	 *Cost-conditions
+	 */
+	
+	@BeforeMethod
+	public void createTestReport(Method method) {
+		System.out.println("Before Method"+method.getName());
+	}
+	
+	@Test(priority=1)
+	@Parameters("BrowserType")
+	public void test(String browser) {
+		System.out.println(browser);
 		assert Boolean.TRUE;
 	}
+	@Test(priority=2)
+	public void test2() {
+		System.out.println("Actual test");
+		assert Boolean.TRUE;
+	}
+	
+	
 	
 }
